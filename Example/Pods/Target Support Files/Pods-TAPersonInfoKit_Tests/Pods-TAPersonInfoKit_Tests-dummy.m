a58fde2507d9054771a70810f3eb1a79ef97ffe4
Alan <zanma@zanmas-Mac-mini.local>
2019-04-02 20:35:57 +0800

    first

---
+++ Example/Pods/Target Support Files/Pods-TAPersonInfoKit_Tests/Pods-TAPersonInfoKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TAPersonInfoKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TAPersonInfoKit_Tests
+@end
---
+++ Example/Pods/Target Support Files/TAPersonInfoKit/TAPersonInfoKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TAPersonInfoKit : NSObject
+@end
+@implementation PodsDummy_TAPersonInfoKit
+@end
